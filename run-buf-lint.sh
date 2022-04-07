e32ad3c29eb844d4618179f92a91e09e0065a892
Shray Kumar <shrayk@gmail.com>
2022-04-07 16:04:22 -0400

    Update run-buf-lint.sh

--- run-buf-lint.sh
+++ run-buf-lint.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 files=$(echo "$@" | tr ' ' ',')
-if [ -z "$files" ]
+
+if [ -z "${files}" ]
 then
   echo "Warning: no proto files changed"
   exit 0
 else
+  echo "Linting: ${files}"
   buf lint --path ${files}
 fi
 
