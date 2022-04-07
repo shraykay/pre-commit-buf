1ca119fc99cc2a34ffc349c743c823072d275e78
Shray Kumar <shrayk@gmail.com>
2022-04-07 16:19:44 -0400

    Update run-buf-lint.sh

--- run-buf-lint.sh
+++ run-buf-lint.sh
@@ -2,12 +2,11 @@
 set -x
 files=$(echo "$@" | tr ' ' ',')
 
-if [ -z "${files}" ]
-then
-  echo "Warning: no proto files changed"
-  exit 0
-else
+if [ $# -gt 0 ]; then
   echo "Linting: ${files}"
   buf lint --path ${files}
+else
+  echo "Warning: no proto files changed"
+  exit 0
 fi
 
